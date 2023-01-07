984ebf1816251e8c86007220085cae4d8bbecb9d
Padaliya Krisha <108071979+Krisha018@users.noreply.github.com>
2023-01-07 20:54:18 +0530

    Create Clock.c

---
+++ lab-2/Clock.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<time.h>
+
+void main()
+{
+	int i;
+	clock_t start,end;
+	double cpu_time_used;
+	start=clock();
+	for(i=0;i<100;i++)
+	{
+		printf("%d\n",i);
+	}
+	end=clock();
+	cpu_time_used=((double)(end-start))/CLOCKS_PER_SEC;
+	printf("\n the time taken is :%lf\n",cpu_time_used);
+}
